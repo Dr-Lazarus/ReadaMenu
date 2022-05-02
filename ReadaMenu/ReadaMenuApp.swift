fa1fdd62f3549e956d49facc680c8ee426f9e204
ShiHui21 <85828941+ShiHui21@users.noreply.github.com>
2022-05-02 11:04:09 +0800

    Initial Commit

---
+++ ReadaMenu/ReadaMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadaMenuApp.swift
+//  ReadaMenu
+//
+//  Created by Luah Shi Hui on 2/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ReadaMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
